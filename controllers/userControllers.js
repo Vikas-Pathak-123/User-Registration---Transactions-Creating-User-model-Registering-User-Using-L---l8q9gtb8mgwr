ccfea4405807d6978e75e3d42aa4f868d71f6d44
Vikas Pathak <123vikas.vp79@gmail.com>
2023-05-02 15:42:13 +0530

    check assignment test case

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -17,10 +17,24 @@ Post request json file structure
 //you will get error if user mail allready exist in that case you need to return 404 status with err message that you get.
 //to look the user schema look ../models/user.js
 
-const registerUser =async (req, res) => {
 
-    //Write you code here
-
-}
-
-module.exports = { registerUser };
+const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+      // Check if user with same email already exists
+      const userExists = await User.findOne({ email });
+      if (userExists) {
+        return res.status(404).json({ error: "User with this email already exists" });
+      }
+  
+      // Create new user
+      const user = new User({ name, email, password });
+      const savedUser = await user.save();
+      res.json({ id: savedUser._id });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  };
+  
+  module.exports = { registerUser };
